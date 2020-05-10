6ee4e1eb565b3be53fe150507d9e5816c79d5d8d
Matias  Cornejo lazcano <matiascornejolazcano@MacBook-Air-de-Matias.local>
2020-05-09 23:58:10 -0400

    probando2

---
+++ Entrega2Proyecto/Entrega2Proyecto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Entrega2Proyecto
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
