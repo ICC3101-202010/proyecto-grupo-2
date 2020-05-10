c625594c2d844cf5875afe93488746cc31b33ffe
AntoniaCarrizo <62261170+AntoniaCarrizo@users.noreply.github.com>
2020-05-09 23:41:16 -0400

    Add files via upload

---
+++ ProyectoGrupo2/ProyectoGrupo2/AbsMaster.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace ProyectoGrupo2
+{
+    
+    public class AbsMaster
+    {
+        protected string genero;
+        protected string añoPublicacion;
+        protected int numeroReproducciones;
+        protected string titulo;
+        protected int duracion;
+        protected int memoria;
+        protected string estudio;
+        protected string palabraClave;
+
+        public AbsMaster() // Alvarin
+        {
+            
+        }
+        // Terminar Metodos.
+        public bool AgregarImagen(string imagen)
+        {
+            return true;
+        }
+        public bool AgregarLike()
+        {
+            return true;
+        }
+        public bool AgregarCalificacion()
+        {
+            return true;
+        }
+        public bool AgregarReproduccion()
+        {
+            return true;
+        }
+        public bool AgregarListaReproduccion()
+        {
+            return true;
+        }
+    }
+}
+//hh
---
+++ ProyectoGrupo2/ProyectoGrupo2/SongClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProyectoGrupo2
+{
+    public class SongClass: AbsMaster
+    {
+        private string _compositor;
+        private string _cantante;
+        private string _discografia;
+        private string _letra; //FUNCION AGREGADA
+        public SongClass(string genero, string añoPublicacion, int numeroReproducciones,
+                     string titulo, int duracion, int memoria, string estudio, string palabraClave,
+                     string compositor, string cantante, string discografia, string letra)
+        {
+            this.genero = genero;
+            this.añoPublicacion = añoPublicacion;
+            this.numeroReproducciones = numeroReproducciones;
+            this.titulo = titulo;
+            this.duracion = duracion;
+            this.memoria = memoria;
+            this.estudio = estudio;
+            this.palabraClave = palabraClave;
+
+            this._compositor = compositor;
+            this._cantante = cantante;
+            this._discografia = discografia;
+
+        }
+        public void AgregarDatos()
+        {
+
+        }
+
+    }
+}
