d789296ff0cf32220f83021f7451c36c28dfd890
AlvaroCespedes <aecespedes@miuandes.cl>
2020-05-10 17:29:05 -0400

    nueva

--- ProyectoEntrega2/Album.cs
+++ ProyectoEntrega2/Album.cs
@@ -5,7 +5,7 @@ namespace ProyectoGrupo2
 {
     public class Album
     {
-        //ALVARO NUEVO
+        //ALVARO NUEVa
         private string nameAlbum;
         private string gender;
         private string producer;
